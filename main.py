46c1e39224d34afe70ab1b1affaa1bcaf63b7b21
[joshua cochran] <waltwest@outlook.com>
2025-05-09 17:28:03 -0400

    .
    .

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-import requests as req
+import requests as req # captures webpages
+import bs4 # parses html?
 
-# use baseballmusings.com
-#  the url is going to need to be dynamic
-res = req.get('https://www.baseballmusings.com/cgi-bin/CurStreak.py?EndDate=05%2F07%2F2025')
+# use baseballmusings.com, returns Response object
+res = req.get('https://www.baseballmusings.com/cgi-bin/CurStreak.py')
 
-# shows html of the page
-print(res.text)
+# takes text of Response object and returns BeautifulSoup object
+# needed for BeautifulSoup.select()
+soup = bs4.BeautifulSoup(res.text, 'html.parser')
+
+# captures wanted elements using CSS selector?
+elems = soup.select('#body > table > tbody > tr:nth-child(2) > td:nth-child(1) > table')
+
+print(len(elems))
+
+# Selector for the table
+#body > table > tbody > tr:nth-child(2) > td:nth-child(1) > table
