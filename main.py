53f959e3eef54bda02ea46a1655f784af2764a42
[joshua cochran] <waltwest@outlook.com>
2025-05-09 18:25:22 -0400

    .
    .

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ res = requests.get('https://www.baseballmusings.com/cgi-bin/CurStreak.py')
 
 # returns BeautifulSoup object
 soup = bs4.BeautifulSoup(res.text, 'html.parser')
-
+soup.prettify()
 # captures table entries
 table_header = soup.select('th') # table headers
 table_contents = soup.select('tr') # table rows
