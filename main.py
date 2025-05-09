d90e42798020aa28d9d6da0428690c0f895998da
[joshua cochran] <waltwest@outlook.com>
2025-05-09 18:05:58 -0400

    .
    .

--- main.py
+++ main.py
@@ -9,19 +9,6 @@ res = req.get('https://www.baseballmusings.com/cgi-bin/CurStreak.py')
 soup = bs4.BeautifulSoup(res.text, 'html.parser')
 
 # captures table entries
-table_header = soup.select('th')
+table_header = soup.select('th') # retrieves table headers
 table_contents = soup.select('tr') # selects elements with <tr> tage which are the table elements
 
-
-print(len(table_header))
-for table_header in table_header:
-    print(table_header.getText())
-exit()
-
-for elems in elems:
-    print(elems.getText())
-
-#print(len(elems))
-
-#for tr in elems:
-    #print(tr)
