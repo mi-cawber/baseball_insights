41206f956d4658a09db0534f65a42486f8500a09
[joshua cochran] <waltwest@outlook.com>
2025-05-10 14:10:10 -0400

    .
    .

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ res = requests.get('https://www.baseballmusings.com/cgi-bin/CurStreak.py')
 soup = bs4.BeautifulSoup(res.text, 'html.parser')
 
 # captures table entries
-table_contents = soup.select('tr') # table rows
+table_contents = soup.select('td') # table rows
 
+
+#first player data is contained from [3]-[14], 13 elements per
+#second is [15]-[28]
+print(table_contents[28].getText())
 # add contents to data.csv file
---
+++ scratch.py
@@ -0,0 +1,14 @@
+'''
+set a switch
+
+while (switch false)
+    
+    if (something to read)
+        iterate 13 times
+        newline
+
+    else
+        switch = true
+
+
+'''
