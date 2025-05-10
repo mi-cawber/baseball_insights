76fb05ff6f3ea36a0cf1e18757871fbc779e5097
[joshua cochran] <waltwest@outlook.com>
2025-05-10 15:06:35 -0400

    .
    .

--- main.py
+++ main.py
@@ -6,7 +6,22 @@ res = requests.get('https://www.baseballmusings.com/cgi-bin/CurStreak.py')
 # returns BeautifulSoup object
 soup = bs4.BeautifulSoup(res.text, 'html.parser')
 
-# captures table entries
-table_contents = soup.select('td') # table rows
+# captures table contents from web
+raw_contents = soup.select('td') # table rows
 
-print(table_contents[457].getText())
+# cuts irrelevant content
+truncated_contents = raw_contents[2:457] # goes from [0]-[454]
+
+print(truncated_contents[12].getText())
+from datetime import datetime, date
+
+today = date.today()
+date_string = today.strftime("%Y-%m-%d")
+
+
+def test():
+    x=0
+    while x <13:
+        print(truncated_contents[x].getText())
+        x +=1
+    print(date_string)
