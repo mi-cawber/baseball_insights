2e7b51239a6a8bd75a0dc51eecc399b14f3c26a0
[joshua cochran] <waltwest@outlook.com>
2025-05-10 14:35:11 -0400

    .
    .

--- main.py
+++ main.py
@@ -9,8 +9,4 @@ soup = bs4.BeautifulSoup(res.text, 'html.parser')
 # captures table entries
 table_contents = soup.select('td') # table rows
 
-
-#first player data is contained from [3]-[14], 13 elements per
-#second is [15]-[28]
-print(table_contents[28].getText())
-# add contents to data.csv file
+print(table_contents[457].getText())
