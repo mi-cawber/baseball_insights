6043e05816aaf4b80b3ccd21f51d545d4918d2cc
[joshua cochran] <waltwest@outlook.com>
2025-05-12 19:43:00 -0400

    .
    .

--- scratch.py
+++ scratch.py
@@ -3,9 +3,8 @@ import requests, bs4
 # get hit streak data from web
 res = requests.get('https://www.baseballmusings.com/cgi-bin/CurStreak.py') #returns response object
 soup = bs4.BeautifulSoup(res.text, 'html.parser') # returns BeautifulSoup object
-raw_contents = soup.select('td') # captures data
-truncated_contents = raw_contents[2:456] # truncates irrelevant data (now range == [0]-[454])
+raw_contents = soup.select('.letter, .number') # captures data
 
-for element in truncated_contents:
+for element in raw_contents:
    
     print(f'{element.get_text()}', end=',') #THIS FUCKING DOES IT
