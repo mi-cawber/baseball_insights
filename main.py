f26eff602dd50b366e0a6e4bdb13b6331b50f474
[joshua cochran] <waltwest@outlook.com>
2025-05-13 19:08:55 -0400

    .
    .

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import functions as f
 
 raw, array = f.retrieve_data()
 f.data_transfer(raw, array) # transfers Tag data to array
-f.array_csv(array,'test.csv') # import into csv file
+f.array_csv(array,'data.csv') # import into csv file
