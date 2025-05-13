22feea57e00f2ce74e10979c1f5fb0962e7cedfd
[joshua cochran] <waltwest@outlook.com>
2025-05-12 20:15:13 -0400

    .
    .

--- scratch.py
+++ scratch.py
@@ -4,16 +4,18 @@ import requests, bs4
 res = requests.get('https://www.baseballmusings.com/cgi-bin/CurStreak.py') #returns response object
 soup = bs4.BeautifulSoup(res.text, 'html.parser') # returns BeautifulSoup object
 raw_contents = soup.select('td .number, td .letter') # captures data
-
-print(len(raw_contents))
-str(raw_contents[415])
-print(raw_contents[415].attrs)
-exit()
-truncated_elements = raw_contents[2:]
+data_array = [] # im gonna put the data in here, easier to manipulate
 
 
 
 
-for element in truncated_elements:
-   
-    print(f'{element.get_text()}', end=',') #THIS FUCKING DOES IT
+# transfers Tag data into array
+def data_transfer():
+    for element in raw_contents:
+        array.append(element.getText())
+        
+# shows data array
+def show_data_array():
+    for element in array:
+        print(element)
+
