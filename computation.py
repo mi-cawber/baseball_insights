fa075e63fd71f0c12f0a087d307252a489e726e3
[joshua cochran] <waltwest@outlook.com>
2025-05-14 22:18:29 -0400

    .
    .

--- computation.py
+++ computation.py
@@ -2,10 +2,5 @@ import pandas as pd, matplotlib as plt
 
 df = pd.read_csv('hit_streaks.csv')
 
-print(df.Games.describe()) # describe
+print(df['Player'].value_counts())
 
-print(df.Player.describe())
-
-print(df.Player.asfreq())
-frequencies = df['Games'].value_counts()
-print(frequencies)
