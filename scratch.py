cab5877fd4147c5428bb7b97f180182019eb9ea2
[joshua cochran] <waltwest@outlook.com>
2025-05-15 20:37:52 -0400

    .
    .

--- scratch.py
+++ scratch.py
@@ -3,4 +3,4 @@ import retrieval_functions as f
 f.date_checker('standings.csv')
 raw, list = f.retrieve_data('https://www.baseballmusings.com/cgi-bin/Standings.py?year=2025')
 f.data_transfer(raw, list)
-f.list_csv(list,'standings.csv') # import into csv file
+f.list_csv(list,'standings.csv') 
