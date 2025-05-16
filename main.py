131afc22adc7e3a67e3e1cf5b825a67fc6f15196
[joshua cochran] <waltwest@outlook.com>
2025-05-16 16:30:35 -0400

    .

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import retrieval_functions as f
 
+# to collect hit streak data
 f.date_checker('hit_streaks.csv')
 raw, list = f.retrieve_data('https://www.baseballmusings.com/cgi-bin/CurStreak.py')
 f.data_transfer(raw, list) # transfers Tag data to list
 f.list_csv(list,'hit_streaks.csv') # import into csv file
+
+# to collect standings data
+f.date_checker('standings.csv')
+raw, list = f.retrieve_data('https://www.baseballmusings.com/cgi-bin/Standings.py?year=2025')
+f.data_transfer(raw, list)
+f.list_csv(list,'standings.csv')
