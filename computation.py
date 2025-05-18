78cd94e72ed9689df31d2382ac461b6be7ca3f89
[joshua cochran] <waltwest@outlook.com>
2025-05-18 19:34:33 -0400

    .

--- computation.py
+++ computation.py
@@ -1,10 +1,20 @@
 import pandas as pd
 
 
-def current_standings():
+
+def win_pct():
+    standings = pd.read_csv('standings.csv')
+    win_pct = standings[['Team', 'Win Pct', 'Date Collected']]
+    win_pct = win_pct.tail(30)
+    win_pct = win_pct.sort_values(['Win Pct'], ascending=False)
+    print(win_pct)
+
+
+def standings():
     standings = pd.read_csv('standings.csv') # standings dataframe
     # first set of brackets accesses dataframe, second set is list of columns
-    standings = standings[['Team','Record']]
+    standings = standings[['Team','Record','Date Collected']]
     print(standings.tail(30)) # 30 teams
 
-current_standings()
+standings()
+win_pct()
