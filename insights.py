eb9c16432c4c12bc6ae8058d61808f3a70b39a29
[joshua cochran] <waltwest@outlook.com>
2025-05-23 23:19:00 -0400

    .

--- insights.py
+++ insights.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 # this will print the current win percentages, according to the latest data
 def win_pct():
-    standings = pd.read_csv('standings.csv')
+    standings = pd.read_csv('standings_2025.csv')
     win_pct = standings[['Team', 'Win Pct', 'Date Collected']]
     win_pct = win_pct.tail(30)
     win_pct = win_pct.sort_values(['Win Pct'], ascending=False)
@@ -16,18 +16,20 @@ def win_pct():
 
 # displays the current standings, according to the latest data
 def standings():
-    standings = pd.read_csv('standings.csv') # standings dataframe
+    standings = pd.read_csv('standings_2025.csv') # standings dataframe
     # first set of brackets accesses dataframe, second set is list of columns
     standings = standings[['Team','Record','Date Collected']]
     print(standings.tail(30)) # 30 teams
 
 # this will display the top hit streak leaders
 def hit_streak_leaders():
-    hit_streaks = pd.read_csv('hit_streaks.csv')
+    hit_streaks = pd.read_csv('hit_streaks_2025.csv')
     # drop all columns except player, streak count
-    trimmed_streaks = hit_streaks[['Player', 'Games']]
+    trimmed_streaks = hit_streaks[['Player', 'Games', 'Date Collected']]
     # this sorts the data by highest hits
     max_streaks = trimmed_streaks.sort_values(['Games'], ascending=False)
     # this drops duplicates of player, leaving only their highest streak
     max_streaks = max_streaks.drop_duplicates(['Player'])
     print(max_streaks)
+
+hit_streak_leaders()
--- main.py
+++ main.py
@@ -4,13 +4,13 @@ Main file, for running retrieval functions.
 import retrieval as r
 
 # to collect hit streak data
-r.date_checker('hit_streaks.csv', 'hit streak')
+r.date_checker('hit_streaks_2025.csv', 'hit streak')
 raw, list = r.retrieve_data('https://www.baseballmusings.com/cgi-bin/CurStreak.py')
 r.data_transrer(raw, list) # transfers Tag data to list
-r.list_csv(list,'hit_streaks.csv') # import into csv file
+r.list_csv(list,'hit_streaks_2025.csv') # import into csv file
 
 # to collect standings data
-r.date_checker('standings.csv', 'standings')
+r.date_checker('standings_2025.csv', 'standings')
 raw, list = r.retrieve_data('https://www.baseballmusings.com/cgi-bin/Standings.py?year=2025')
 r.data_transrer(raw, list)
-r.list_csv(list,'standings.csv')
+r.list_csv(list,'standings_2025.csv')
