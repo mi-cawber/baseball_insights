1c50830149f0f72d2afd46fc7e98ac85e9059608
mi-cawber <106614898+mi-cawber@users.noreply.github.com>
2025-05-25 17:36:50 -0400

    .

--- scratch.py
+++ scratch.py
@@ -1,7 +1,4 @@
-'''
-This file is for experimentation.
-'''
-import pandas as pd
+import insights as i, pandas as pd
 
 print('Choose one of the following: \n' \
 '1) Collect data \n' \
@@ -14,5 +11,13 @@ if response == 1:
     print('hello')
 
 if response == 2:
-    print('goodbye')
+    print('What do you want to see? \n' \
+    '1) Standings\n' \
+    '2) Win pct ranked\n' \
+    '3) Hit streak leaders')
 
+    choice = int(input())
+
+    if choice == 1:
+        print('Standings:\n')
+        i.standings()
