d891798c8209c917761d062436d35d458f65b929
[joshua cochran] <waltwest@outlook.com>
2025-05-26 13:30:31 -0400

    .

--- scratch2.py
+++ scratch2.py
@@ -1,8 +1,14 @@
 import requests, bs4, csv
 
 
+# returns Response object
 response = requests.get('https://www.fangraphs.com/leaders/major-league?pagenum=1&pageitems=2000000000')
 
-print(response.text)
+# use bs4 to turn into BeautifulSoup object
+soup = bs4.BeautifulSoup(response.text, 'html.parser')
 
+# select data we want using bs4.select()
+# returns ResultSet
+raw = soup.select('tbody')
 
+print(type(raw))
