abad84fa14a8d69eb2639f1fb08126bdab05a299
mi-cawber <waltwest@outlook.com>
2025-08-02 09:58:51 -0400

    working on pi integration

---
+++ piscript.py
@@ -0,0 +1,14 @@
+import retrieval as r, insights as i
+
+# to collect hit streak data
+r.date_checker('hit_streaks_2025.csv', 'hit streak')
+raw, list = r.retrieve_data('https://www.baseballmusings.com/cgi-bin/CurStreak.py')
+r.data_transfer(raw, list) # transfers Tag data to list
+r.list_csv(list,'hit_streaks_2025.csv') # import into csv file
+
+# to collect standings data
+r.date_checker('standings_2025.csv', 'standings')
+raw, list = r.retrieve_data('https://www.baseballmusings.com/cgi-bin/Standings.py?year=2025')
+r.data_transfer(raw, list)
+r.list_csv(list,'standings_2025.csv')
+
