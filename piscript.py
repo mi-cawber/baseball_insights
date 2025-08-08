e57650b61f898a1047cc20f6fd8e05d3e35793f5
mi-cawber <waltwest@outlook.com>
2025-08-08 14:05:02 -0400

    auto-commit

--- piscript.py
+++ piscript.py
@@ -1,4 +1,9 @@
 import retrieval as r
+import os
+
+#for cron daemon. This will set the working directory as the project folder,
+#so that relative paths can be used in functions.
+os.chdir('/home/joshua/baseball_insights')
 
 # hit streak data
 r.scraping_pipeline('https://www.baseballmusings.com/cgi-bin/CurStreak.py', 'hit_streaks_2025.csv', 'hit streak')
